8e74cdd9e23125e53a5a2892bc29c5ff04383270
areisde <alexandre@unki.ch>
2025-07-11 00:18:44 +0200

    Last try on AWS

--- backend/main.py
+++ backend/main.py
@@ -9,6 +9,4 @@ app.include_router(ingest_router)
 app.include_router(retrieve_router)
 app.include_router(crawl_router)
 
-if __name__ == "__main__":
-    import uvicorn
-    uvicorn.run(app, host="0.0.0.0", port=8000)
+handler = Mangum(app)
