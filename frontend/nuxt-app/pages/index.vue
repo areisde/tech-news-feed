5486f50295d22320c8a9d9018bd25ddc6862de0a
areisde <alexandre@unki.ch>
2025-07-11 12:20:05 +0200

    Added frontend support

---
+++ frontend/nuxt-app/pages/index.vue
@@ -0,0 +1,7 @@
+<template>
+  <div>
+    <h1>Welcome to Tech News Feed</h1>
+    <p>This is your home page.</p>
+    <UButton>Click Me</UButton>
+  </div>
+</template>
---
+++ frontend/nuxt-app/tailwind.config.js
@@ -0,0 +1,13 @@
+// tailwind.config.js
+module.exports = {
+  content: [
+    './pages/**/*.{vue,js,ts}',
+    './components/**/*.{vue,js,ts}',
+    './app.vue',
+    './node_modules/@nuxthq/ui/dist/**/*.js'
+  ],
+  theme: {
+    extend: {},
+  },
+  plugins: [],
+}
