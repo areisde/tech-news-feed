d9f8b8fbee37845303d6c958fd0e9f491c25d9d5
areisde <alexandre@unki.ch>
2025-07-11 17:28:12 +0200

    Platform foundation

--- frontend/nuxt-app/pages/index.vue
+++ frontend/nuxt-app/pages/index.vue
@@ -1,9 +1,24 @@
 <script setup>
-    import ArticleList from '~/components/ArticleList.vue'
-    const config = useRuntimeConfig()
-    const { data, error } = await useFetch('/api/retrieve')
+import { ar } from '@nuxt/ui/runtime/locale/index.js'
 
-    console.log("Data fetched:", data)
+    const articles = ref([])
+
+    const fetch_articles = async () => {
+      try {
+        const response = await $fetch('/api/retrieve')
+        console.log('Fetched articles:', response)
+        articles.value = Array.isArray(response) ? response : (response.articles || [])
+      } catch (err) {
+        console.error('Error fetching articles:', err)
+        articles.value = []
+      }
+    }
+
+    onMounted(async () => {
+      await fetch_articles()
+      console.log('Articles on mount:', articles.value)
+    })
+
 </script>
 
 
@@ -11,11 +26,19 @@
   <header class="bg-[rgba(20,31,97,1)]">
     <img src="public/assets/images/nexthink_logo.svg" alt="Nexthink Logo" class="h-20 p-6 inline-block align-middle" />
     <h1 class="text-3xl font-extralight text-white mb-4 inline-block border-l-2 border-white align-middle pl-4 mt-4">IT Latest</h1>
+    <UButton
+      class="cursor-pointer float-end mt-5 me-6"
+      icon="i-lucide-refresh-ccw"
+      label="Refresh"
+      color="neutral"
+      size="xl"
+      @click="fetch_articles"
+    />
+
   </header>
-  <body>
-    
-    <ArticleList :articles="data ?? []" />
-    <div v-if="error">Error loading articles.</div>
-    <div v-else-if="!data">Loading...</div>
+  <body class="bg-gray-200 h-[calc(100vh_-_5rem)] overflow-y-auto">
+    <div class="p-4">
+      <ArticleList :articles="articles" />
+    </div>
   </body>
 </template>
